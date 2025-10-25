154fe9cf6236c7dc3d464b3c5d96b7bfed7bbde8
dev-brunosantos <brunosdesenvolvedor@gmail.com>
2025-10-25 06:55:15 -0300

    Adicionando o novo context de alunos no arquivo principal de contextos

--- src/contexts/AppContexts.tsx
+++ src/contexts/AppContexts.tsx
@@ -1,9 +1,12 @@
+import { AlunosContextProvider } from "./AlunosContexts"
 import { LoginContextProvider } from "./LoginContext"
 
 export const AppContext = ({ children }: { children: React.ReactNode }) => {
     return (
         <LoginContextProvider>
-            {children}
+            <AlunosContextProvider>
+                {children}
+            </AlunosContextProvider>
         </LoginContextProvider>
     )
 }
